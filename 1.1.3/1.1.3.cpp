8481781edffce6a12d13a91b877f26b1f227f611
monkeyking <jackschora@gmail.com>
2016-11-20 18:10:29 +0800

    Fill the bubblesort1A

--- 1.1.3/1.1.3.cpp
+++ 1.1.3/1.1.3.cpp
@@ -9,7 +9,49 @@ using namespace std;
 
 int main()
 {
-	cout<<"hello";
+	void bubblesort1A(int A[], int n);
+	int A[] = {5,2,7,4,6,3,1};
+	void print(int A[]);
+	print(A);
+	bubblesort1A(A, 7);
+	print(A);
 	system("pause");
     return 0;
+}
+
+void print(int A[]) {
+	for (int i = 0; i < 7; i++)
+	{
+		cout << A[i];
+		cout << "  ";
+		if (i == 6)
+		{
+			cout << endl;
+		}
+	}
+}
+
+void bubblesort1A(int A[], int n) {
+	bool sorted = false;
+	void swap(int*a,int*b);
+	void print(int A[]);
+	while (!sorted)
+	{
+		sorted = true;
+		for (int i = 1; i < n; i++)
+		{
+			if (A[i - 1] > A[i]) {
+				swap(&A[i-1],&A[i]);
+				sorted = false;
+			}
+		}
+		n--;
+	}
+}
+
+
+void swap(int *a, int *b) {
+	*a = *b + *a;
+	*b = *a - *b;
+	*a = *a - *b;
 }
